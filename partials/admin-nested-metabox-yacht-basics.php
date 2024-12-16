8b2220fc74836c220810e962abe0255ea3ba2e8f
Brandon Creed <brandon.f.creed@gmail.com>
2024-12-16 15:52:04 -0500

    feature/distance_fields cleaned up styling on other yacht info to match changes on yacht info

--- partials/admin-nested-metabox-yacht-basics.php
+++ partials/admin-nested-metabox-yacht-basics.php
@@ -58,137 +58,122 @@
     }
 
     $images = get_post_meta( $post->ID, 'Images', true );
-
-   // var_dump($images);
-/*
-    $yacht_ysp_image_1 = $images[0]->Uri;
-    $yacht_ysp_image_2 = $images[1]->Uri;
-    $yacht_ysp_image_3 = $images[2]->Uri;
-    $yacht_ysp_image_4 = $images[3]->Uri;
-    $yacht_ysp_image_5 = $images[4]->Uri;
-    $yacht_ysp_image_6 = $images[5]->Uri;
-    $yacht_ysp_image_7 = $images[6]->Uri;
-    $yacht_ysp_image_8 = $images[7]->Uri;
-    $yacht_ysp_image_9 = $images[8]->Uri;
-    $yacht_ysp_image_10 = $images[9]->Uri;
-    $yacht_ysp_image_11 = $images[10]->Uri;
-    $yacht_ysp_image_12 = $images[11]->Uri;
-    $yacht_ysp_image_13 = $images[12]->Uri;
-    $yacht_ysp_image_14 = $images[13]->Uri;
-    $yacht_ysp_image_15 = $images[14]->Uri;
-    $yacht_ysp_image_16 = $images[15]->Uri;
-    $yacht_ysp_image_17 = $images[16]->Uri;
-    $yacht_ysp_image_18 = $images[17]->Uri;
-    $yacht_ysp_image_19 = $images[18]->Uri;
-    $yacht_ysp_image_20 = $images[19]->Uri;*/
-
 ?>
 
 
 <div id="yacht-nested-metabox-basics">
-    <div class="metafield">
-        <label>General Boat Description</label>
-        <textarea name="General_Boat_Description" style="width: 100%; height: 300px;">
-            <?php if (isset($yacht_ysp_general_boat_description[0])) : ?>
-                <?= $yacht_ysp_general_boat_description[0] ?>
-            <?php else: ?>
-
-            <?php endif; ?>
-        </textarea>
-    </div>
-    <div class="metafield">
-        <label>YSP Sales Rep Party ID</label>
-        <input type="text" name="YSP_Sales_Rep_Party_ID" value="<?= $yacht_ysp_sales_rep_party_id ?>">
-    </div>
-    <div class="metafield">
-        <label>YSP Sales Rep Name</label>
-        <input type="text" name="YSP_Sales_Rep_Name" value="<?= $yacht_ysp_sales_rep_name ?>">
-    </div>
-    <div class="metafield">
-        <label>YSP Engine 1 Make</label>
-        <input type="text" name="YSP_Engine_1_Make" value="<?= $yacht_ysp_engine_1_make ?>">
-    </div>
-    <div class="metafield">
-        <label>YSP Engine 1 Model</label>
-        <input type="text" name="YSP_Engine_1_Model" value="<?= $yacht_ysp_engine_1_model ?>">
-    </div>
-    <div class="metafield">
-        <label>YSP Engine 1 Fuel</label>
-        <input type="text" name="YSP_Engine_1_Fuel" value="<?= $yacht_ysp_engine_1_fuel ?>">
-    </div>
-    <div class="metafield">
-        <label>YSP Engine 1 Engine Power</label>
-        <input type="text" name="YSP_Engine_1_EnginePower" value="<?= $yacht_ysp_engine_1_engine_power ?>">
-    </div>
-    <div class="metafield">
-        <label>YSP Engine 1 Type</label>
-        <input type="text" name="YSP_Engine_1_Type" value="<?= $yacht_ysp_engine_1_type ?>">
-    </div>
-    <div class="metafield">
-        <label>YSP Engine 1 Year</label>
-        <input type="text" name="YSP_Engine_1_Year" value="<?= $yacht_ysp_engine_1_year ?>">
-    </div>
-    <div class="metafield">
-        <label>YSP Engine 1 Hours</label>
-        <input type="text" name="YSP_Engine_1_Hours" value="<?= $yacht_ysp_engine_1_hours ?>">
-    </div>
-    <div class="metafield">
-        <label>YSP Engine 1 Boat Engine Location Code</label>
-        <input type="text" name="YSP_Engine_1_Boat_Engine_Location_Code" value="<?= $yacht_ysp_engine_1_boat_engine_location_code ?>">
-    </div>
-    <div class="metafield">
-        <label>YSP Engine 2 Make</label>
-        <input type="text" name="YSP_Engine_2_Make" value="<?= $yacht_ysp_engine_2_make ?>">
-    </div>
-    <div class="metafield">
-        <label>YSP Engine 2 Model</label>
-        <input type="text" name="YSP_Engine_2_Model" value="<?= $yacht_ysp_engine_2_model ?>">
-    </div>
-    <div class="metafield">
-        <label>YSP Engine 2 Fuel</label>
-        <input type="text" name="YSP_Engine_2_Fuel" value="<?= $yacht_ysp_engine_2_fuel ?>">
-    </div>
-    <div class="metafield">
-        <label>YSP Engine 2 Engine Power</label>
-        <input type="text" name="YSP_Engine_2_EnginePower" value="<?= $yacht_ysp_engine_2_engine_power ?>">
-    </div>
-    <div class="metafield">
-        <label>YSP Engine 2 Type</label>
-        <input type="text" name="YSP_Engine_2_Type" value="<?= $yacht_ysp_engine_2_type ?>">
-    </div>
-    <div class="metafield">
-        <label>YSP Engine 2 Year</label>
-        <input type="text" name="YSP_Engine_2_Year" value="<?= $yacht_ysp_engine_2_year ?>">
-    </div>
-    <div class="metafield">
-        <label>YSP Engine 2 Hours</label>
-        <input type="text" name="YSP_Engine_2_Hours" value="<?= $yacht_ysp_engine_2_hours ?>">
-    </div>
-    <div class="metafield">
-        <label>YSP Engine 2 Boat Engine Location Code</label>
-        <input type="text" name="YSP_Engine_2_Boat_Engine_Location_Code" value="<?= $yacht_ysp_engine_2_boat_engine_location_code ?>">
-    </div>
-
-    <?php for($x = 0; $x <= 19; $x++) { 
-        if (isset($images[ ($x) ]->Uri)) : 
-            $v = $images[ ($x) ]->Uri;
-        else:
-            $v = '';
-        endif;
-        ?>
-        <div class="metafield">
-            <label>YSP Image <?= $x ?></label>
-            
-            <input type="text" name="YSP_Image_<?= $x ?>" value="<?= $v ?>">
-
-            <input type="button" value="Or Upload" data-name="YSP_Image_<?= $x ?>" class="ysp-manual-images-uploads">
+    <fieldset class="field-group">
+        <legend>General Details</legend>
+        <div class="grid-container">
+            <div class="metafield">
+                <label>Sales Rep Name</label>
+                <input type="text" name="YSP_Sales_Rep_Name" value="<?= $yacht_ysp_sales_rep_name ?>">
+            </div>
+            <div class="metafield">
+                <label>Sales Rep Party ID</label>
+                <input type="text" name="YSP_Sales_Rep_Party_ID" value="<?= $yacht_ysp_sales_rep_party_id ?>">
+            </div>
+            <div class="metafield metafield-full">
+                <label>General Boat Description</label>
+                <textarea name="General_Boat_Description" style="width: 100%; height: 300px;">
+                    <?php if (isset($yacht_ysp_general_boat_description[0])) : ?>
+                        <?= $yacht_ysp_general_boat_description[0] ?>
+                    <?php endif; ?>
+                </textarea>
+            </div>
         </div>
-    <?php } ?>
+    </fieldset>
+    <fieldset class="field-group">
+        <legend>Engine 1 Details</legend>
+        <div class="grid-container">
+            <div class="metafield">
+                <label>Engine 1 Make</label>
+                <input type="text" name="YSP_Engine_1_Make" value="<?= $yacht_ysp_engine_1_make ?>">
+            </div>
+            <div class="metafield">
+                <label>Engine 1 Model</label>
+                <input type="text" name="YSP_Engine_1_Model" value="<?= $yacht_ysp_engine_1_model ?>">
+            </div>
+            <div class="metafield">
+                <label>Engine 1 Fuel</label>
+                <input type="text" name="YSP_Engine_1_Fuel" value="<?= $yacht_ysp_engine_1_fuel ?>">
+            </div>
+            <div class="metafield">
+                <label>Engine 1 Engine Power</label>
+                <input type="text" name="YSP_Engine_1_EnginePower" value="<?= $yacht_ysp_engine_1_engine_power ?>">
+            </div>
+            <div class="metafield">
+                <label>Engine 1 Type</label>
+                <input type="text" name="YSP_Engine_1_Type" value="<?= $yacht_ysp_engine_1_type ?>">
+            </div>
+            <div class="metafield">
+                <label>Engine 1 Year</label>
+                <input type="text" name="YSP_Engine_1_Year" value="<?= $yacht_ysp_engine_1_year ?>">
+            </div>
+            <div class="metafield">
+                <label>Engine 1 Hours</label>
+                <input type="text" name="YSP_Engine_1_Hours" value="<?= $yacht_ysp_engine_1_hours ?>">
+            </div>
+            <div class="metafield">
+                <label>Engine 1 Location Code</label>
+                <input type="text" name="YSP_Engine_1_Boat_Engine_Location_Code" value="<?= $yacht_ysp_engine_1_boat_engine_location_code ?>">
+            </div>
+        </div>
+    </fieldset>
+    <fieldset class="field-group">
+        <legend>Engine 2 Details</legend>
+        <div class="grid-container">
+            <div class="metafield">
+                <label>Engine 2 Make</label>
+                <input type="text" name="YSP_Engine_2_Make" value="<?= $yacht_ysp_engine_2_make ?>">
+            </div>
+            <div class="metafield">
+                <label>Engine 2 Model</label>
+                <input type="text" name="YSP_Engine_2_Model" value="<?= $yacht_ysp_engine_2_model ?>">
+            </div>
+            <div class="metafield">
+                <label>Engine 2 Fuel</label>
+                <input type="text" name="YSP_Engine_2_Fuel" value="<?= $yacht_ysp_engine_2_fuel ?>">
+            </div>
+            <div class="metafield">
+                <label>Engine 2 Engine Power</label>
+                <input type="text" name="YSP_Engine_2_EnginePower" value="<?= $yacht_ysp_engine_2_engine_power ?>">
+            </div>
+            <div class="metafield">
+                <label>Engine 2 Type</label>
+                <input type="text" name="YSP_Engine_2_Type" value="<?= $yacht_ysp_engine_2_type ?>">
+            </div>
+            <div class="metafield">
+                <label>Engine 2 Year</label>
+                <input type="text" name="YSP_Engine_2_Year" value="<?= $yacht_ysp_engine_2_year ?>">
+            </div>
+            <div class="metafield">
+                <label>Engine 2 Hours</label>
+                <input type="text" name="YSP_Engine_2_Hours" value="<?= $yacht_ysp_engine_2_hours ?>">
+            </div>
+            <div class="metafield">
+                <label>Engine 2 Location Code</label>
+                <input type="text" name="YSP_Engine_2_Boat_Engine_Location_Code" value="<?= $yacht_ysp_engine_2_boat_engine_location_code ?>">
+            </div>
+        </div>
+    </fieldset>
+
+    <fieldset class="field-group">
+        <legend>Images</legend>
+        <div class="grid-container">
+            <?php for ($x = 0; $x <= 19; $x++) {
+                $v = isset($images[$x]->Uri) ? $images[$x]->Uri : ''; ?>
+                <div class="metafield">
+                    <label>Image <?= $x ?></label>
+                    <input type="text" name="YSP_Image_<?= $x ?>" value="<?= $v ?>">
+                    <input type="button" value="Or Upload" data-name="YSP_Image_<?= $x ?>" class="ysp-manual-images-uploads">
+                </div>
+            <?php } ?>
+        </div>
+    </fieldset>
 </div>
 
 <script type="text/javascript">
-
-    // jQuery().each(function() {jQuery(this)
     jQuery('#yacht-nested-metabox-basics .ysp-manual-images-uploads').each(function() {
     
         jQuery(this).on('click', function(e) {
@@ -224,10 +209,9 @@
 
                 let field = document.querySelector('#yacht-nested-metabox-basics input[name="'+ name +'"]');
 
-                field.value = url; //set which variable you want the field to have
+                field.value = url;
             });
 
-            // Finally, open the modal
             file_frame.open();
         })
     });
@@ -236,7 +220,7 @@
 
 
 <style type="text/css">
-	#yacht-nested-metabox-basics .metafield{
+	/* #yacht-nested-metabox-basics .metafield{
 		display: block;
 		margin-top: 10px;
 		margin-bottom: 10px;
@@ -257,5 +241,55 @@
 	}
     #yacht-nested-metabox-basics .metafield input[type="button"]{
         margin-top: 20px;
+    } */
+    #yacht-nested-metabox-basics {
+        font-family: Arial, sans-serif;
+    }
+
+    .field-group {
+        margin-bottom: 20px;
+        padding: 15px;
+        border: 1px solid #ddd;
+        border-radius: 5px;
+    }
+
+    .field-group legend {
+        font-size: 16px;
+        font-weight: bold;
+        margin-bottom: 10px;
+    }
+
+    .grid-container {
+        display: grid;
+        grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
+        gap: 20px;
+    }
+
+    .metafield {
+        margin-bottom: 15px;
+    }
+
+    .metafield-full {
+        grid-column: span 2;
+    }
+
+    .metafield label {
+        display: block;
+        margin-bottom: 5px;
+        font-weight: bold;
+    }
+
+    .metafield input,
+    .metafield textarea {
+        width: 100%;
+        padding: 5px;
+        border: 1px solid #ccc;
+        border-radius: 3px;
+    }
+
+    .metafield input[type="button"] {
+        margin-top: 10px;
+        padding: 5px 10px;
+        cursor: pointer;
     }
 </style>

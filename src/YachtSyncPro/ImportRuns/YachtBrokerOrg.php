57faecd62aacd111e640efc307311264aee8c4a2
Brandon Creed <brandon.f.creed@gmail.com>
2025-02-14 15:09:20 -0500

    feature/odyssey_marine added change to stop mass deletion of boats on sync

--- src/YachtSyncPro/ImportRuns/YachtBrokerOrg.php
+++ src/YachtSyncPro/ImportRuns/YachtBrokerOrg.php
@@ -318,6 +318,9 @@
 	                    ],
 	                ]);
 
+					$pdf_still_e = false;
+		           	$yacht_updated = false;
+
 		           	if (! isset($find_post_from_synced[0]->ID)) {
 			            if (! empty($record['BoatHullID'])) {
 			                $find_post_from_synced=get_posts([
@@ -428,8 +431,8 @@
 
 					wp_set_post_terms($y_post_id, $theBoat['MakeString'], 'boatmaker', false);
 					wp_set_post_terms($y_post_id, $theBoat['BoatClassCode'], 'boatclass', false);
-					//wp_set_post_terms($y_post_id, $theBoat['BoatCategoryCode'], 'boattype', false);
-					//wp_set_post_terms($y_post_id, $theBoat['SaleClassCode'], 'boatcondition', false);
+					wp_set_post_terms($y_post_id, $theBoat['BoatCategoryCode'], 'boattype', false);
+					wp_set_post_terms($y_post_id, $theBoat['SaleClassCode'], 'boatcondition', false);
 
 					if ($this->opt_prerender_brochures == 'yes' && $pdf_still_e == false && ! in_array($theBoat['SalesStatus'], ['Sold', 'Suspend']) ) {
 
